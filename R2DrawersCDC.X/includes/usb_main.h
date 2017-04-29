8065cdafa5e14f2149f4928eaf9c0e49b380ccab
Eric Lei <el536@cornell.edu>
2017-04-29 16:44:47 -0400

    Usb code works!
    Works!

--- R2DrawersCDC.X/includes/usb_main.h
+++ R2DrawersCDC.X/includes/usb_main.h
@@ -4,11 +4,12 @@
 #define _USB_MAIN
 
 #include <stdint.h>
+#include "R2Protocol.h"
 
 #define BUFFER_LENGTH   255
 
 //int ProcessIO(char* sourceBuffer, char* payloadBuffer, char* checksumBuffer, char* transactionBuffer);
-char ProcessIO(char * buffer);
+int ProcessIO(struct R2ProtocolPacket *packet);
 
 uint16_t readIndex, writeIndex;
 uint8_t readBuffer[BUFFER_LENGTH];
